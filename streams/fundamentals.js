2b14d88b1020ec0782bb70f9b586d63b5fe80f67
James Meneses <james.meneseswk@gmail.com>
2024-07-29 19:09:51 -0300

    Sets a Writable and a Transform Stream and put it on the pipe

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,5 +1,6 @@
-import { Readable } from "node:stream";
+import { Readable, Transform, Writable } from "node:stream";
 
+// Lê o dado e passa para o próximo stream
 class OneToHundredStream extends Readable {
   index = 1;
 
@@ -17,4 +18,22 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+class NegativeNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+// Processa o dado
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new NegativeNumberStream())
+  .pipe(new MultiplyByTenStream());
